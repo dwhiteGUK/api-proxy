e2ba1997de14073784f1407def2893e380a01b72
Darren White <dwhite@gentingcasinos.co.uk>
2022-10-19 15:55:29 +0100

    refactor: :recycle: Add Access-Control-Allow-Origin for CORs

---
+++ next.config.js
@@ -0,0 +1,16 @@
+// next.config.js
+module.exports = {
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*.amplifyapp.com" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  }
+};
