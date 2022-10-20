d2b933224f880affe9056ae1d0e60894fb15275f
Darren White <dwhite@gentingcasinos.co.uk>
2022-10-20 11:54:04 +0100

    feat: :sparkles: Allow CORs

--- pages/api/proxy.js
+++ pages/api/proxy.js
@@ -1,3 +1,20 @@
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
 const options = {
   method: 'GET',
   headers: {
@@ -25,4 +42,4 @@ async function handler(req, res) {
   }
 }
 
-export default handler
+export default allowCors(handler)
