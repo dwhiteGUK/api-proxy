6d219af7dade1c451914a564663865f19cfa0720
Darren White <dwhite@gentingcasinos.co.uk>
2022-10-20 12:16:30 +0100

    refactor: :recycle: Restrict proxy if calling from browser

--- pages/api/proxy.js
+++ pages/api/proxy.js
@@ -1,9 +1,18 @@
 const allowCors = fn => async (req, res) => {
+  console.log('🚀 ~ file: proxy.js ~ line 4 ~ allowCors ~ req.headers.origin', req.headers.origin)
+
+  if (req.headers.origin !== undefined && !req.headers.origin.includes('d21mhytekt7r3f.amplifyapp.com') && !req.headers.origin.includes('http://localhost:')) {
+    res.status(405).end()
+    return
+  }
+
+  const origin = req.headers.origin || '*'
+
   res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Origin', origin)
   // another common pattern
   // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,POST')
   res.setHeader(
     'Access-Control-Allow-Headers',
     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
